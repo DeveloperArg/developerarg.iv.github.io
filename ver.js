d4963705b36a481e9c03f3f94501b9b31f19a198
DeveloperArg <111541526+DeveloperArg@users.noreply.github.com>
2022-08-24 19:58:00 -0300

    Add files via upload

---
+++ ver.js
@@ -0,0 +1,27 @@
+var cerrar=new Image(256, 256);
+cerrar.src="img/cerrar.png";
+
+var abrir=new Image(256, 256);
+menu.src="img/menu-icon.png";
+
+function visualizar()
+{	
+	if(barraresponsiva.style.visibility=="hidden"){
+	document.images["ver"].src=menu.src;
+	/*document.images["ver"].style.right="0px";*/
+	barraresponsiva.style.visibility="visible";
+	contentmenu.style.visibility="hidden";
+	menu.style.visibility="hidden";
+	cuerpo.style.overflow="scroll";
+
+}else{document.images["ver"].src=cerrar.src;
+		/*document.images["ver"].style.right="15px";*/
+	barraresponsiva.style.visibility="hidden";
+	contentmenu.style.visibility="visible";
+	menu.style.visibility="visible";
+	cuerpo.style.overflow="hidden";
+}
+
+}
+nodoVer=document.getElementById("ver");
+nodoVer.setAttribute("onclick", "visualizar();");
